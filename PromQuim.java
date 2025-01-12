71995f40758d4e515d7cdbe3c49846b6436a0087
Joel Acosta <garen123ap@gmail.com>
2025-01-12 11:30:05 -0500

    Trabajo

---
+++ PromQuim.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PromQuim{
+	public static void main(String [] args) {
+		float numUno, numDos, promNotas;
+		Scanner entrada= new Scanner(System.in);
+		System.out.println("Ingrese la primera nota");
+		numUno=entrada.nextFloat();
+	        System.out.println("Ingrese la segunda nota");	
+		numDos=entrada.nextFloat();
+		promNotas= (numUno+numDos)/2;
+		
+		if (promNotas>=5) {
+			System.out.println("Su promedio es :  "  + promNotas);
+			System.out.println("Usted pasa el año directamente");
+		}		
+		else if (promNotas<5){
+			System.out.println("Su promedio es :" + promNotas);
+			System.out.println("Usted esta suspenso");
+		}
+		
+	}
+}
---
+++ Promedio.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class PromQuim{
+	public static void main(String [] args) {
+		float numUno, NumDos, promNotas;
+	}
+}
