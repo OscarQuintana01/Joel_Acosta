817c4de528c08a4ce1e67d361eff107718946798
Joel Acosta <garen123ap@gmail.com>
2025-01-12 09:34:29 -0500

    Primer triangulo

---
+++ PerimetroRec.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PerimetroRec{
+	public static void main (String [] args) {
+	double base, altura, perimetro;
+		Scanner entrada= new Scanner(System.in);
+		System.out.println("Ingrese el valor");
+		base=entrada.nextdouble();
+		System.out.println("Ingrese la altura");
+		altura=entrada.nextdouble();
+		perimetro= 2(base+altura);	
+		System.out.println("El perimetro es" + perimetro);	
+	
+	}
+}
